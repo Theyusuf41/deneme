847d601c997346c625a117ae1273807ec96a09a0
Theyusuf41 <yusufakrglu@gmail.com>
2023-03-23 14:42:29 +0100

    Yusuf

---
+++ src/yusuf/Arrays.java
@@ -0,0 +1,10 @@
+package yusuf;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] arr = {1,23,4,5,6};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
